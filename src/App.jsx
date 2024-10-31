7aa38b3a0ea18c403072388e7621accd9e6d4d60
mparente88 <parente.molly@gmail.com>
2024-10-31 14:44:26 -0700

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,24 @@ import { useState } from "react"
 import IngredientList from "./components/IngredientList"
 import BurgerStack from "./components/BurgerStack"
 
-export const availableIngredients = [
-  { name: "Kaiser Bun", color: "saddlebrown" },
-  { name: "Sesame Bun", color: "sandybrown" },
-  { name: "Gluten Free Bun", color: "peru" },
-  { name: "Lettuce Wrap", color: "olivedrab" },
-  { name: "Beef Patty", color: "#3F250B" },
-  { name: "Soy Patty", color: "#3F250B" },
-  { name: "Black Bean Patty", color: "#3F250B" },
-  { name: "Chicken Patty", color: "burlywood" },
-  { name: "Lettuce", color: "lawngreen" },
-  { name: "Tomato", color: "tomato" },
-  { name: "Bacon", color: "maroon" },
-  { name: "Onion", color: "lightyellow" },
-  { name: "Cheddar Cheese", color: "#FDE18B" },
-  { name: "Swiss Cheese", color: "#F1E1A8" },
-]
-
 const App = () => {
+  const availableIngredients = [
+    { name: "Kaiser Bun", color: "saddlebrown" },
+    { name: "Sesame Bun", color: "sandybrown" },
+    { name: "Gluten Free Bun", color: "peru" },
+    { name: "Lettuce Wrap", color: "olivedrab" },
+    { name: "Beef Patty", color: "#3F250B" },
+    { name: "Soy Patty", color: "#3F250B" },
+    { name: "Black Bean Patty", color: "#3F250B" },
+    { name: "Chicken Patty", color: "burlywood" },
+    { name: "Lettuce", color: "lawngreen" },
+    { name: "Tomato", color: "tomato" },
+    { name: "Bacon", color: "maroon" },
+    { name: "Onion", color: "lightyellow" },
+    { name: "Cheddar Cheese", color: "#FDE18B" },
+    { name: "Swiss Cheese", color: "#F1E1A8" },
+  ]
+
   const [stack, setStack] = useState([])
 
   const addToBurger = (ingredient) => {
